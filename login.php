6b59bfc8e12ef73491ce0a0118f2f02ca3c111a0
guitar61111886 <guitarthunrada@gmail.com>
2021-05-25 05:36:17 +0700

    T.T

--- login.php
+++ login.php
@@ -1,20 +1,23 @@
-<?php 
+<?php
 session_start();
-include('server.php'); 
+include('server.php');
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login Page</title>
 
-    <link rel="stylesheet" href="style.css"
+    <link rel="stylesheet" href="style.css">
 </head>
 
 
 <body>
+
+
     <div class="header">
         <h2>Login</h2>
     </div>
@@ -23,14 +26,14 @@ include('server.php');
         <?php if (isset($_SESSION['error'])) : ?>
             <div class="error">
                 <h3>
-                    <?php 
-                        echo $_SESSION['error'];
-                        unset($_SESSION['error']);
+                    <?php
+                    echo $_SESSION['error'];
+                    unset($_SESSION['error']);
                     ?>
                 </h3>
             </div>
         <?php endif ?>
-    
+
         <div class="input-group">
             <label for="username"> Username </label>
             <input type="text" name="username">
